d7e68aea9944e2001b7ab1267ac11a62f9e5defe
wubbzy25 <carlosasalas321@gmail.com>
2023-08-07 14:14:03 -0500

    Pequeño cambio

--- index.js
+++ index.js
@@ -2,73 +2,120 @@ const chatbotToggler = document.querySelector(".chatbot-toggler");
 const closeBtn = document.querySelector(".close-btn");
 const chatbox = document.querySelector(".chatbox");
 const chatInput = document.querySelector(".chat-input textarea");
+const clientBtn = document.querySelector(".client-btn");
+const advisorBtn = document.querySelector(".advisor-btn");
+const clientForm = document.querySelector(".client-form");
+const advisorForm = document.querySelector(".advisor-form")
+const headerChat = document.querySelector("header h2");
+const InputChat = document.querySelector(".chatbot .chat-input");
+const incomingChat = document.querySelector(".chatbox .incoming span");
+const pChat = document.querySelector(".chatbox .chat p");
+const spanChat = document.querySelector(".chatbot header span");
+const formclientBtn = document.querySelector(".buton");
+const formadvisorBtn = document.querySelector(".buton2");
 const sendChatBtn = document.querySelector(".chat-input span");
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+
+const showClientForm = () => {
+    clientForm.style.display = "block";
+    document.querySelector(".buttons").classList.add("hidden");
+    document.querySelector(".text").classList.add("hidden");
+};
+
+clientBtn.addEventListener("click", showClientForm)
+clientForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+});
+
+const showAdvisorForm = () => {
+    advisorForm.style.display = "block";
+    document.querySelector(".buttons").classList.add("hidden");
+    document.querySelector(".text").classList.add("hidden");
+};
+
+advisorBtn.addEventListener("click", showAdvisorForm)
+advisorForm.addEventListener("submit", function (event) {
+    event.preventDefault()
+});
+
+const showChatBot = () => {
+    headerChat.style.display = "block";
+    spanChat.style.display = "block";
+    pChat.style.display = "block";
+    clientForm.style.display = "none";
+    advisorForm.style.display = "none";
+    InputChat.style.display = "flex"
+};
+
+formclientBtn.addEventListener("click", showChatBot);
+formadvisorBtn.addEventListener("click", showChatBot);
 const inputInitHeight = chatInput.scrollHeight;
+
 const createChatLi = (message, className) => {
     // Create a chat <li> element with passed message and className
     const chatLi = document.createElement("li");
-    chatLi.classList.add("chat", `${className}`);
+    chatLi.classList.add("chat2", `${className}`);
     let chatContent = className === "outgoing" ? `<p></p>` : `<span class="material-symbols-outlined">smart_toy</span><p></p>`;
     chatLi.innerHTML = chatContent;
     chatLi.querySelector("p").textContent = message;
     return chatLi; // return chat <li> element
 }
-const generateResponse = (chatElement) => {
-    const API_URL = "https://api.openai.com/v1/chat/completions";
-    const messageElement = chatElement.querySelector("p");
-    // Define the properties and message for the API request
-    const requestOptions = {
-        method: "POST",
-        headers: {
-            "Content-Type": "application/json",
-            "Authorization": `Bearer ${API_KEY}`
-        },
-        body: JSON.stringify({
-            model: "gpt-3.5-turbo",
-            messages: [{role: "user", content: userMessage}],
-        })
-    }
-    // Send POST request to API, get response and set the reponse as paragraph text
-    fetch(API_URL, requestOptions).then(res => res.json()).then(data => {
-        messageElement.textContent = data.choices[0].message.content.trim();
-    }).catch(() => {
-        messageElement.classList.add("error");
-        messageElement.textContent = "Oops! Something went wrong. Please try again.";
-    }).finally(() => chatbox.scrollTo(0, chatbox.scrollHeight));
-}
+
+
 const handleChat = () => {
     userMessage = chatInput.value.trim(); // Get user entered message and remove extra whitespace
-    if(!userMessage) return;
+    if (!userMessage) return;
     // Clear the input textarea and set its height to default
     chatInput.value = "";
     chatInput.style.height = `${inputInitHeight}px`;
     // Append the user's message to the chatbox
     chatbox.appendChild(createChatLi(userMessage, "outgoing"));
     chatbox.scrollTo(0, chatbox.scrollHeight);
-    
+
+    // Simulate a response from the bot based on user's input
     setTimeout(() => {
-        // Display "Thinking..." message while waiting for the response
-        const incomingChatLi = createChatLi("Thinking...", "incoming");
+        const botResponse = getBotResponse(userMessage);
+        const incomingChatLi = createChatLi(botResponse, "incoming");
         chatbox.appendChild(incomingChatLi);
         chatbox.scrollTo(0, chatbox.scrollHeight);
-        generateResponse(incomingChatLi);
     }, 600);
 }
+
 chatInput.addEventListener("input", () => {
     // Adjust the height of the input textarea based on its content
     chatInput.style.height = `${inputInitHeight}px`;
     chatInput.style.height = `${chatInput.scrollHeight}px`;
 });
+
 chatInput.addEventListener("keydown", (e) => {
     // If Enter key is pressed without Shift key and the window 
     // width is greater than 800px, handle the chat
-    if(e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
+    if (e.key === "Enter" && !e.shiftKey && window.innerWidth > 800) {
         e.preventDefault();
         handleChat();
     }
 });
+
 sendChatBtn.addEventListener("click", handleChat);
 closeBtn.addEventListener("click", () => document.body.classList.remove("show-chatbot"));
 chatbotToggler.addEventListener("click", () => document.body.classList.toggle("show-chatbot"));
+
+// Function to get a bot response based on user's input
+const getBotResponse = (userMessage) => {
+    // Replace this logic with your own FAQs and response generation
+    // Example: If userMessage is "What is your name?", return "My name is ChatBot."
+    // You can define an object or array with questions and their corresponding answers.
+    const faqs = {
+        "What is your name?": "Hi, My name is Kira.",
+        "Connect me with a advisor": "Connecting with an advisor...",
+        "How can I contact support?": "You can contact support by emailing support@example.com.",
+        // Add more FAQs and responses here
+    };
+
+    // Check if user's message is a known question
+    if (faqs.hasOwnProperty(userMessage)) {
+        return faqs[userMessage];
+    } else {
+        return "I'm sorry, I don't have an answer for that question.";
+    }
+};
